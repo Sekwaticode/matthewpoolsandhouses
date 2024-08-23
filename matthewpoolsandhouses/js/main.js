e9011fb0eec87ed3a0ce63fd3502d5d96f8e3613
Sekwaticode <lsekwati2@gmail.com>
2024-08-23 17:05:26 +0200

    redone

--- matthewpoolsandhouses/js/main.js
+++ matthewpoolsandhouses/js/main.js
@@ -1,112 +1,137 @@
 (function ($) {
-  "use strict";
+    "use strict";
+    
+    // Initiate the wowjs
+    new WOW().init();
+    
+    
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 200) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
+        return false;
+    });
+    
+    
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 90) {
+            $('.nav-bar').addClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "73px");
+        } else {
+            $('.nav-bar').removeClass('nav-sticky');
+            $('.carousel, .page-header').css("margin-top", "0");
+        }
+    });
+    
+    
+    // Dropdown on mouse hover
+    $(document).ready(function () {
+        function toggleNavbarMethod() {
+            if ($(window).width() > 992) {
+                $('.navbar .dropdown').on('mouseover', function () {
+                    $('.dropdown-toggle', this).trigger('click');
+                }).on('mouseout', function () {
+                    $('.dropdown-toggle', this).trigger('click').blur();
+                });
+            } else {
+                $('.navbar .dropdown').off('mouseover').off('mouseout');
+            }
+        }
+        toggleNavbarMethod();
+        $(window).resize(toggleNavbarMethod);
+    });
+    
+    
+    // jQuery counterUp
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+    
+    
+    // Modal Video
+    $(document).ready(function () {
+        var $videoSrc;
+        $('.btn-play').click(function () {
+            $videoSrc = $(this).data("src");
+        });
+        console.log($videoSrc);
 
-  // Spinner
-  var spinner = function () {
-    setTimeout(function () {
-      if ($("#spinner").length > 0) {
-        $("#spinner").removeClass("show");
-      }
-    }, 1);
-  };
-  spinner();
+        $('#videoModal').on('shown.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0");
+        })
 
-  // Initiate the wowjs
-  new WOW().init();
+        $('#videoModal').on('hide.bs.modal', function (e) {
+            $("#video").attr('src', $videoSrc);
+        })
+    });
 
-  // Sticky Navbar
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
-    } else {
-      $(".sticky-top").removeClass("shadow-sm").css("top", "-100px");
-    }
-  });
 
-  // Back to top button
-  $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-      $(".back-to-top").fadeIn("slow");
-    } else {
-      $(".back-to-top").fadeOut("slow");
-    }
-  });
-  $(".back-to-top").click(function () {
-    $("html, body").animate({ scrollTop: 0 }, 1500, "easeInOutExpo");
-    return false;
-  });
+    // Testimonial Slider
+    $('.testimonial-slider').slick({
+        infinite: true,
+        autoplay: true,
+        arrows: false,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.testimonial-slider-nav'
+    });
+    $('.testimonial-slider-nav').slick({
+        arrows: false,
+        dots: false,
+        focusOnSelect: true,
+        centerMode: true,
+        centerPadding: '22px',
+        slidesToShow: 3,
+        asNavFor: '.testimonial-slider'
+    });
+    $('.testimonial .slider-nav').css({"position": "relative", "height": "160px"});
+    
+    
+    // Blogs carousel
+    $(".related-slider").owlCarousel({
+        autoplay: true,
+        dots: false,
+        loop: true,
+        nav : true,
+        navText : [
+            '<i class="fa fa-angle-left" aria-hidden="true"></i>',
+            '<i class="fa fa-angle-right" aria-hidden="true"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            576:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
+    });
+    
+    
+    // Portfolio isotope and filter
+    var portfolioIsotope = $('.portfolio-container').isotope({
+        itemSelector: '.portfolio-item',
+        layoutMode: 'fitRows'
+    });
 
-  // Testimonials carousel
-  $(".testimonial-carousel").owlCarousel({
-    autoplay: true,
-    smartSpeed: 1000,
-    items: 1,
-    dots: false,
-    loop: true,
-    nav: true,
-    navText: [
-      '<i class="bi bi-chevron-left"></i>',
-      '<i class="bi bi-chevron-right"></i>',
-    ],
-  });
+    $('#portfolio-flters li').on('click', function () {
+        $("#portfolio-flters li").removeClass('filter-active');
+        $(this).addClass('filter-active');
+
+        portfolioIsotope.isotope({filter: $(this).data('filter')});
+    });
+    
 })(jQuery);
 
-/*-----Service Tabs-----*/
-
-// Dom Elements: Tabs
-const constructionTab = document.getElementById("constructionService");
-const electricalBackupTab = document.getElementById("electricalBackupService");
-const electricianServiceTab = document.getElementById(
-  "electricanServiceContainer"
-);
-const solarPanelTab = document.getElementById("solarPanelInstallation");
-
-// Dom Elements: Button
-const constructionButton = document.getElementById("constructionServiceButton");
-const electricalBackupButton = document.getElementById(
-  "electricalBackupButton"
-);
-const electricianServiceButton = document.getElementById(
-  "electrianServicesButton"
-);
-const solarPanelButton = document.getElementById(
-  "solarPanelInstallationButton"
-);
-
-function displayConstructionTab() {
-  constructionTab.style.display = "block";
-  electricalBackupTab.style.display = "none";
-  electricianServiceTab.style.display = "none";
-  solarPanelTab.style.display = "none";
-
-}
-
-function displayElectricalBackup() {
-  constructionTab.style.display = "none";
-  electricalBackupTab.style.display = "block";
-  electricianServiceTab.style.display = "none";
-  solarPanelTab.style.display = "none";
-}
-
-function displayElectricianServices() {
-  constructionTab.style.display = "none";
-  electricalBackupTab.style.display = "none";
-  electricianServiceTab.style.display = "block";
-  solarPanelTab.style.display = "none";
-}
-
-function displaySolarPanels() {
-  constructionTab.style.display = "none";
-  electricalBackupTab.style.display = "none";
-  electricianServiceTab.style.display = "none";
-  solarPanelTab.style.display = "block";
-}
-
-document.addEventListener("DOMContentLoaded", () => {
-  displayConstructionTab();
-});
-
-constructionButton.addEventListener("click", displayConstructionTab);
-electricalBackupButton.addEventListener("click", displayElectricalBackup);
-electricianServiceButton.addEventListener("click", displayElectricianServices);
-solarPanelButton.addEventListener("click", displaySolarPanels);
